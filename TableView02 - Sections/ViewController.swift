a1c78edc0e648b4f3cecdb041338541b23e0de8d
ZeusMode <cintra.wesley@gmail.com>
2016-10-23 22:04:21 -0200

    first commit

--- TableView02 - Sections/ViewController.swift
+++ TableView02 - Sections/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "clubes", ofType: "plist")
+        let namesDict = NSDictionary(contentsOfFile: path!)
+        
+        names = namesDict as! [String: [String]]
+        keys = (namesDict?.allKeys as! [String]).sorted()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: - Propriedades
+    let idCell = "Cell"
+    var names: [String: [String]]!
+    var keys: [String]!
+    
+    // MARK: - TableView Datasources
+    // numero de seções
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return keys!.count
+    }
+    
+    // numero de linhas
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let key = keys[section]
+        let nameSection = names[key]!
+        
+        return nameSection.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return keys[section]
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: idCell)
+        
+        let key = keys[indexPath.section]
+        let nameSection = names[key]!
+        
+        cell.textLabel?.text = nameSection[indexPath.row]
+        
+        return cell
+    }
+    
+    // Implementar um indice de secoes
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return keys
+    }
 }
 
